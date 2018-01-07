30cdf6fd72546ba100e34203e05634c482d63f2c
Neil <30810497+NeilW-M@users.noreply.github.com>
2018-01-07 16:35:19 -0500

    Add files via upload

---
+++ main/java/com/eppingforesters/titaniummod/init/ItemInit.java
@@ -0,0 +1,16 @@
+package com.eppingforesters.titaniummod.init;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.eppingforesters.titaniummod.objects.items.ItemBase;
+
+import net.minecraft.item.Item;
+
+public class ItemInit {
+
+	public static final List<Item> ITEMS = new ArrayList<Item>();
+	
+	public static final Item TITANIUM_INGOT = new ItemBase("ingot_titanium");
+	
+}
