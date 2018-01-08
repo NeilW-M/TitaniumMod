ba03780838e621629b7bd20d2babc6b7033a1321
Chase Duffy <31660207+scduffy@users.noreply.github.com>
2018-01-07 20:46:31 -0500

    Add files via upload

--- main/java/com/eppingforesters/titaniummod/init/ItemInit.java
+++ main/java/com/eppingforesters/titaniummod/init/ItemInit.java
@@ -12,5 +12,6 @@ public class ItemInit {
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	
 	public static final Item INGOT_TITANIUM = new ItemBase("ingot_titanium");
+	public static final Item INGOT_TUNGSTEN = new ItemBase("ingot_tungsten");
 	
 }
