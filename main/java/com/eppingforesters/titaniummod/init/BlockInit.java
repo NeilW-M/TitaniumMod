0bf4b751a6a5a6c85999276532460a9db43fed04
Neil <30810497+NeilW-M@users.noreply.github.com>
2018-01-08 16:12:12 -0500

    Add files via upload

--- main/java/com/eppingforesters/titaniummod/init/BlockInit.java
+++ main/java/com/eppingforesters/titaniummod/init/BlockInit.java
@@ -13,6 +13,6 @@ public class BlockInit {
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
 
 	public static final Block BLOCK_TITANIUM = new BlockBase("block_titanium", Material.IRON);
-	public static final Block BLOCK_TUNGSTEN = new BlockBase("block_tungsten", Material.IRON);
+	public static final Block BLOCK_OSMIUM = new BlockBase("block_osmium", Material.IRON);
 
 }
